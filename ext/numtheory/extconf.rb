ac84c6ebd1b2bf93cc7397f026bd7f453c245cb0
lomereiter <lomereiter@gmail.com>
2013-05-24 12:58:22 +0400

    added 2.0.0 to travis * removed -Wno-unused-but-set-variable flag

--- ext/numtheory/extconf.rb
+++ ext/numtheory/extconf.rb
@@ -1,3 +1,3 @@
 require 'mkmf'
-$CFLAGS = "-g -O3 -funroll-all-loops -fPIC -pedantic -std=c99 -Wall -Wno-unused-but-set-variable"
+$CFLAGS = "-g -O3 -funroll-all-loops -fPIC -pedantic -std=c99 -Wall"
 create_makefile 'numtheory'
